28251e36f1af77dcd22db35bdd53574c2bacd8aa
alexander16108 <alexandermayowa05@gmail.com>
2021-11-17 11:29:43 +0100

    add books to store

--- src/components/bookStore.js
+++ src/components/bookStore.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from './navbar';
 import AddBook from './addBooks';
 import Categories from './Categories';
 import BookList from './bookList';
 
 const Bookstore = () => (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route exact path="/" element={<BookList />} />
-      <Route exact path="/" element={<AddBook />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
-  </Router>
+  <Provider>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<BookList />} />
+        <Route exact path="/" element={<AddBook />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default Bookstore;
