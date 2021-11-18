23c2d6f3fa4ce5ed10ca8ba06afb6c9c811a960b
alexander16108 <alexandermayowa05@gmail.com>
2021-11-18 12:25:21 +0100

    fixing linter errors

--- src/components/option.js
+++ src/components/option.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
-import { removeBookAPI } from '../API/Api';
+import { removeBookAPI } from '../Api/Api';
 
 const Options = ({ id }) => {
   const dispatch = useDispatch();
